c4f09ae75f1c79d4d3d32ba5659d5f9f4f54ee4b
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-10 13:49:49 +0530

    book-itemComponent update.

--- src/app/pages/book-item/book-item.component.ts
+++ src/app/pages/book-item/book-item.component.ts
@@ -11,14 +11,16 @@ import { CartService } from '../../service/user-cart/cart.service';
 })
 export class BookItemComponent {
   @Input() book:any;
+  private qty : number = 0;
 
   constructor(private cartService:CartService) {}
 
   add():void {
+    this.qty = (this.qty<this.book.qtyOnHand)? ++this.qty : this.book.qtyOnHand;
     const item = {
       book: this.book,
-      qty: 1,
-      price: this.book.price
+      qty: this.qty,
+      price: this.book.price * this.qty
     }
     this.cartService.addToCart(item);
   }
