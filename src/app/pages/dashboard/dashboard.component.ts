2591af3e93743c1448c62165c61afa7c567ab031
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-10 12:58:38 +0530

    site-dashboard

---
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -0,0 +1,28 @@
+import { CommonModule, NgFor, NgIf } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { BookItemComponent } from "../book-item/book-item.component";
+
+@Component({
+  selector: 'app-dashboard',
+  standalone: true,
+  imports: [FormsModule,CommonModule,NgIf,NgFor,BookItemComponent],
+  templateUrl: './dashboard.component.html',
+  styleUrl: './dashboard.component.css'
+})
+export class DashboardComponent {
+  private baseUrl : String = "http://localhost:8080";
+  public latestBookList:any;
+
+  constructor(private http:HttpClient){
+    this.loadLatestBook();  
+  }
+
+  loadLatestBook(): void {
+    this.http.get(`${this.baseUrl}/book/latest/7`).subscribe(data=>{
+      this.latestBookList = data;
+    })
+  }
+
+}
