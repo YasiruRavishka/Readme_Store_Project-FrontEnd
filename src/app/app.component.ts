19438e47985767b16ea3f2e8fceff92846aa5c8b
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-10 12:53:24 +0530

    appComponent update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./common/header/header.component";
+import { FooterComponent } from "./common/footer/footer.component";
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Readme_Store_Project-FrontEnd';
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,49 @@
 import { Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { SearchPageComponent } from './pages/search-page/search-page.component';
+import { UserLoginComponent } from './pages/user/user-login/user-login.component';
+import { UserSignupComponent } from './pages/user/user-signup/user-signup.component';
+import { UserProfileComponent } from './pages/user/user-profile/user-profile.component';
+import { UserInventoryComponent } from './pages/user/user-inventory/user-inventory.component';
+import { UserCartComponent } from './pages/user/user-cart/user-cart.component';
+import { UserOrdersComponent } from './pages/user/user-orders/user-orders.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: "",
+        component: DashboardComponent
+    },
+    {
+        path: "search",
+        component: SearchPageComponent
+    },
+    {
+        path: "about",
+        component: AboutComponent
+    },
+    {
+        path: "user/login",
+        component: UserLoginComponent
+    },
+    {
+        path: "user/signup",
+        component: UserSignupComponent
+    },
+    {
+        path: "user/profile",
+        component: UserProfileComponent
+    },
+    {
+        path: "user/inventory",
+        component: UserInventoryComponent
+    },
+    {
+        path: "user/cart",
+        component: UserCartComponent
+    },
+    {
+        path: "user/orders",
+        component: UserOrdersComponent
+    }
+];
