b0cb8f80ab04af3d5ae1dad8554b3473ffdae3ad
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-10 13:03:12 +0530

    user-inventory

---
+++ src/app/pages/user/user-inventory/user-inventory.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-inventory',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-inventory.component.html',
+  styleUrl: './user-inventory.component.css'
+})
+export class UserInventoryComponent {
+
+}
