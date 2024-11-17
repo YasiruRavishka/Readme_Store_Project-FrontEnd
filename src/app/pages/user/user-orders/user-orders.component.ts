011ee85174447ced3526e6f95b718093491afe9b
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-17 17:11:27 +0530

    previousOrders view.

--- src/app/pages/user/user-orders/user-orders.component.ts
+++ src/app/pages/user/user-orders/user-orders.component.ts
@@ -1,12 +1,27 @@
+import { NgFor, NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { AccountService } from '../../../service/user-account/account.service';
 
 @Component({
   selector: 'app-user-orders',
   standalone: true,
-  imports: [],
+  imports: [NgIf, NgFor],
   templateUrl: './user-orders.component.html',
   styleUrl: './user-orders.component.css'
 })
 export class UserOrdersComponent {
+  public orderList : any;
+  public currentOrderItems : any;
 
+  constructor(private accountService:AccountService) {
+    this.orderList = accountService.savedUser.orders;
+  }
+
+  isEmptyList(): boolean {
+    return !this.orderList || this.orderList.length == 0;
+  }
+
+  setCurrentOrderItems(orderItems:any): void {
+    this.currentOrderItems = orderItems;
+  }
 }
