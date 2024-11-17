3a6c5eba0e5c70ebc26ec7a8465abe84fb1a70b1
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-17 18:19:31 +0530

    removed NgIf in appComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,12 @@ import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./common/header/header.component";
 import { FooterComponent } from "./common/footer/footer.component";
-import { NgIf } from '@angular/common';
 import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, NgIf],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
