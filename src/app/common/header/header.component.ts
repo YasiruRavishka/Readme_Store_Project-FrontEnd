2461317aae8976a29be9bc26bf3c80e160530084
YasiruRavishka <YasiruRavishka47@gmail.com>
2024-11-22 21:27:43 +0530

    header-component update

--- src/app/common/header/header.component.ts
+++ src/app/common/header/header.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import { AccountService } from '../../service/user-account/account.service';
 import { NgIf } from '@angular/common';
+import { UserType } from '../../enum/UserType';
 
 @Component({
   selector: 'app-header',
@@ -10,11 +11,37 @@ import { NgIf } from '@angular/common';
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
-  constructor(private accountService:AccountService) { }
+export class HeaderComponent implements OnInit{
+  public btnSwitchName : String = "Switch to admin";
+
+  constructor(private router:Router,private accountService:AccountService) { }
+
+  ngOnInit(): void {
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationEnd) {
+        if (event.url.toLowerCase().startsWith("/admin")) {
+          this.btnSwitchName = "Switch to user";
+        } else {
+          this.btnSwitchName = "Switch to admin";
+        }
+      }
+    });
+  }
 
   isLogged(): Boolean {
     return this.accountService.savedUser != null;
+  }
+
+  isAdmin(): Boolean {
+    return this.isLogged() && this.accountService.savedUser.type == UserType.ADMIN;
+  }
+
+  switchTo() {
+    if (this.btnSwitchName === "Switch to admin") {
+      this.router.navigate(["admin"]);
+    } else {
+      this.router.navigate([""]);
+    }
   }
 
   logout(): void {
